6448a95eb5a68eb44f52427aa15e195af62314b9
andrewmk <andrew@hedges.me.uk>
2015-11-02 21:44:26 +0000

    Update mqtt-publish.py

--- mqtt-publish.py
+++ mqtt-publish.py
@@ -7,10 +7,10 @@ def mtPacket(cmd, variable, payload):
 def mtpConnect(name):
   return mtPacket(
            0b00010000,
-           mtStr("MQTT")  /*protocol name*/ +
-           b'\x04'        /*protocol level*/ +
-           b'\x00'        /*connect flag*/ +
-           b'\xFF\xFF',   /*Keepalive*/
+           mtStr("MQTT") + # protocol name
+           b'\x04' +       # protocol level
+           b'\x00' +       # connect flag
+           b'\xFF\xFF',    # keepalive
            mtStr(name)
   )
 
@@ -21,25 +21,28 @@ def mtpPub(topic, data):
   return  mtPacket(0b00110001, mtStr(topic), data)
 
 import socket
-addr = socket.getaddrinfo("mqttbroker.com", 1883)[0][4]
+import binascii
+import time
+
+addr = socket.getaddrinfo("test.mosquitto.org", 1883)[0][4]
 s = socket.socket()
 
 print('Connecting...')
 s.connect(addr)
 s.send(mtpConnect("WiPy1"))
+print(binascii.hexlify(s.recv(4096)))
 
 print("Publishing...")
-s.send(mtpPub("topic/subtopic", b'my-data'))
+
+time.sleep(10)
+
+s.send(mtpPub("wipytopic", b'wipy-data'))
+
+time.sleep(10)
+
+s.send(mtpPub("wipytopic", b'wipy-data-again'))
+##print(binascii.hexlify(s.recv(4096)))
 
 print('Disconnecting...')
 s.send(mtpDisconnect())
 s.close()
-
-#client.on('data',
-#  print("[MQTT]"+ubinascii.hexlify(data))
-#)
-#
-#client.on('end', 
-#  print('client disconnected')
-#  clearInterval(intr)
-#)
